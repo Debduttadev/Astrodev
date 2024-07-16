d6e66338474511812ada9e86206167daeb3dbf87
Debduttadev <Kaupia@yahoo.com>
2024-07-16 18:55:15 +0530

    seo xml file manage section done , seo status indicator installed banner text upload done 3

--- resources/views/admin/seodetails.blade.php
+++ resources/views/admin/seodetails.blade.php
@@ -20,12 +20,18 @@
             $sitemap=URL::to('/').'/'.'sitemap.xml';
             @endphp
             @if (file_exists(public_path('/') . '/' . 'sitemap.xml'))
-            <a href="{{ $sitemap }}" class="btn btn-primary mt-4 mb-4" target="_blank">See website Sitemap</a>
+            <a href="{{ $sitemap }}" class="btn btn-primary mt-4 mb-4" target="_blank">See website's Sitemap</a>
+            @endif
+            @if (file_exists(public_path('/') . '/' . 'robots.txt'))
+            @php
+            $sitemap=URL::to('/').'/'.'robots.txt';
+            @endphp
+            <a href="{{ $sitemap }}" class="btn btn-primary mt-4 mb-4" target="_blank">See website's Robots.txt</a>
             @endif
             <form id="xmlform" method="post" action="{{ URL::to('xmlupload') }}" enctype="multipart/form-data">
                 @csrf
                 <div class="input-group">
-                    <input class="form-control" type="file" id="formFilexml" name="sitemap" accept=".xml" required>
+                    <input class="form-control" type="file" id="formFilexml" name="sitemap" accept=".xml,.txt" required>
                     <button class="btn btn-outline-secondary" type="submit">Upload sitemap</button>
                 </div>
             </form>
