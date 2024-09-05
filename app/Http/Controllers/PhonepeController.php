9d3496c9aa1602bac60e333ac4819ac950d6d8d9
Debduttadev <Kaupia@yahoo.com>
2024-09-05 18:47:11 +0530

    test payment integration 26

--- app/Http/Controllers/PhonepeController.php
+++ app/Http/Controllers/PhonepeController.php
@@ -153,7 +153,7 @@ class PhonepeController extends Controller
 
         $finalXHeader = hash('sha256', '/pg/v1/status/' . $input['merchantId'] . '/' . $input['transactionId'] . $saltKey) . '###' . $saltIndex;
 
-        $response = Curl::to('https://api.phonepe.com/apis/hermes/pg/v1/status/' . $input['merchantId'] . '/' . $input['transactionId'])
+        $response = Curl::to('https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/status/' . $input['merchantId'] . '/' . $input['transactionId'])
             ->withHeader('Content-Type:application/json')
             ->withHeader('accept:application/json')
             ->withHeader('X-VERIFY:' . $finalXHeader)
