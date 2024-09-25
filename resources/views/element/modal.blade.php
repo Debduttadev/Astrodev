d6dba1d331422b4fd1a359a4e4de50e64c2c9958
Debduttadev <Kaupia@yahoo.com>
2024-09-25 19:00:59 +0530

    front design fix 11

--- resources/views/element/modal.blade.php
+++ resources/views/element/modal.blade.php
@@ -586,7 +586,7 @@
                             <textarea name="homedescription" class="form-control form-control-user"
                                 id="abouthomedescription" aria-describedby="description6"
                                 placeholder="Enter blog description fro home page..." required cols="5"
-                                value="{{$abouthomedescription}}">{{$abouthomedescription}}</textarea>
+                                value="ssddsdsdsd">ssddsdsdsd</textarea>
                         </div>
 
 
